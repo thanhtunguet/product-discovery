fe6172f2cace949b31ab199cf15e14d1da7e851c
Travis CI <ht@thanhtunguet.info>
2019-12-30 21:57:04 +0000

    Deploy to GitHub Pages: 42c70de101da6dce83f35e20f7f5f7766e9057e4

---
+++ precache-manifest.947e1228d9309ef1bd0fb8e8fd3f38cd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bacdc6d3df96c0e589fa257e733b182",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f347beaf07605827a487",
+    "url": "/static/css/3.f04f5a0d.chunk.css"
+  },
+  {
+    "revision": "ebaf7011fc9b59021eb7",
+    "url": "/static/css/5.1b52a8af.chunk.css"
+  },
+  {
+    "revision": "83ff61401b0317c67cea",
+    "url": "/static/css/6.5a6e9ef4.chunk.css"
+  },
+  {
+    "revision": "ba278fcc8eb778b9dbf6",
+    "url": "/static/css/main.6dab26fb.chunk.css"
+  },
+  {
+    "revision": "211f5656eb5566b5e8e5",
+    "url": "/static/js/0.5a4269a5.chunk.js"
+  },
+  {
+    "revision": "3df54bba2137ec524f3fb39f2c61461a",
+    "url": "/static/js/0.5a4269a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f347beaf07605827a487",
+    "url": "/static/js/3.53b5bf90.chunk.js"
+  },
+  {
+    "revision": "413434914cd9492a5be5e4ccc2066611",
+    "url": "/static/js/3.53b5bf90.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e5871490dcb6af8111e",
+    "url": "/static/js/4.e7c33ce5.chunk.js"
+  },
+  {
+    "revision": "ebaf7011fc9b59021eb7",
+    "url": "/static/js/5.4887aa9c.chunk.js"
+  },
+  {
+    "revision": "83ff61401b0317c67cea",
+    "url": "/static/js/6.758b828c.chunk.js"
+  },
+  {
+    "revision": "ba278fcc8eb778b9dbf6",
+    "url": "/static/js/main.321b9ef7.chunk.js"
+  },
+  {
+    "revision": "1acab21a97cf734f643b",
+    "url": "/static/js/runtime-main.814f0533.js"
+  }
+]);
