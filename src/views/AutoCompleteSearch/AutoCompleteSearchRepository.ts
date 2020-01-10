748589f963d9daed6de46fce6c801c51db960ebc
thanhtunguet <thanhtung.uet@gmail.com>
2020-01-10 23:51:16 +0700

    update base url

--- src/views/AutoCompleteSearch/AutoCompleteSearchRepository.ts
+++ src/views/AutoCompleteSearch/AutoCompleteSearchRepository.ts
@@ -4,7 +4,7 @@ import { Repository } from 'repositories/Repository';
 export class AutoCompleteSearchRepository extends Repository {
     constructor() {
         super();
-        this.setBaseURL(window.location.origin);
+        this.setBaseURL(process.env.NODE_ENV === 'development' ? window.location.origin : 'http://123.31.32.172:5001');
     }
 
     public search(query: string): Promise<any> {
